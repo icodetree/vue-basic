c844187fbbe0023995636b2e409732f873fc70b8
icodetree <icodetree@naver.com>
2023-01-14 23:05:08 +0900

    add modify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/databinding">Data</router-link> |
-    <router-link to="/html">Html</router-link>
+    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/databinding">Data</router-link> | -->
+    <!-- <router-link to="/html">Html</router-link> -->
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
