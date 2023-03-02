7f5a17b93d6630388b37d556ab44274a8c6dbb6e
icodetree <icodetree@naver.com>
2023-03-02 23:35:44 +0900

    add app

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <!-- <router-link to="/databinding">Data</router-link> | -->
-    <!-- <router-link to="/html">Html</router-link> -->
+    <router-link to="/databinding">Data</router-link> |
+    <router-link to="/html">Html</router-link>
   </nav>
   <router-view />
 </template>
